d064d12e6ee775212b78e4ea752e1bc013765e52
Spencer Lyon <spencerlyon2@gmail.com>
2013-08-02 10:55:35 -0700

    METHODS: added utils for the methods section; '

---
+++ code/xdress/samplerc.py
@@ -0,0 +1,23 @@
+package = 'package'
+packagedir = 'output'
+sourcedir = 'src'
+
+plugins = ('xdress.stlwrap', 'xdress.autoall', 'xdress.autodescribe',
+           'xdress.cythongen', 'foopack.barplug')
+
+## Which stl containers we need for this code
+stlcontainers = [('vector', 'float64'),
+                 ('set', 'int'),
+                 ('map', 'int', ('map', ('vector', 'uint'), ('set', 'char'))),
+                 ('vector', ('vector', 'float64')),
+                 ('set', 'FooClassBar')
+                 ]
+
+## Which classes to create wrappers for.
+classes = [('FooClass', 'Foo'),
+           ('FooClass', 'Bar', 'Foo', 'FooClassBar'),
+           ]
+
+functions = [('FooFunc', 'Foo')]
+
+Variables = [('barVar', 'Bar')]
